3725a43226d377088e4cca8b2c829062d2b56b92
SunnyXtrem <Sunny1109r@icloud.com>
2024-10-01 16:39:36 +0200

    exercise 4

---
+++ exercise_04.py
@@ -0,0 +1,8 @@
+# 1. Define the function to convert Celsius to Fahrenheit
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (9/5) * celsius + 32
+    return fahrenheit
+
+# 2. Call the function for celsius = 25 and celsius = 10 and print the results
+print("25°C in Fahrenheit:", celsius_to_fahrenheit(25))
+print("10°C in Fahrenheit:", celsius_to_fahrenheit(10))
