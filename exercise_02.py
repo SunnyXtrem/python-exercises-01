102697a71c5deef88e12b91acfcb004acf04691e
SunnyXtrem <Sunny1109r@icloud.com>
2024-10-01 15:04:37 +0200

    exercise 2

---
+++ exercise_02.py
@@ -0,0 +1,21 @@
+# exercise_02.py
+
+# Definiere zwei Ganzzahlen
+a = 10
+b = 3
+
+# Berechnungen
+summe = a + b
+differenz = a - b
+produkt = a * b
+ganzzahliger_quotient = a // b
+rest = a % b
+hoch = a ** b
+
+# Ergebnisse ausgeben
+print("Summe:", summe)
+print("Differenz:", differenz)
+print("Produkt:", produkt)
+print("Ganzzahliger Quotient:", ganzzahliger_quotient)
+print("Rest der Division:", rest)
+print("a hoch b:", hoch)
